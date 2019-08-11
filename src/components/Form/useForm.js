d1c12e143350720f2b683848db912f9f396ca3fb
Łukasz Warchoł <lukasz.warchol14@gmail.com>
2019-08-11 13:37:28 +0200

    added validate form

--- src/components/Form/useForm.js
+++ src/components/Form/useForm.js
@@ -2,19 +2,30 @@ import { useState } from "react";
 
 const useForm = initial => {
   const [values, setValues] = useState(initial);
-  const [error, setError] = useState("");
+  const [error, setError] = useState(null);
 
   const updateValue = e => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
 
-  const validate = values => {};
+  const validate = values => {
+    if (!values.name) return "Enter your name";
+    if (!values.email) return "Enter your email";
+    if (!values.password) return "Enter your password";
+
+    return null;
+  };
 
   const submitHandler = async e => {
     e.preventDefault();
 
     if (validate(values)) setError(validate(values));
-    else console.log(values);
+    else {
+      setError(null);
+
+      //   here you can send your "values"
+      console.log(values);
+    }
   };
 
   return [updateValue, submitHandler, error];
