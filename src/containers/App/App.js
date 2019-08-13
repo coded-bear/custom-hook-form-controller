2a11debcc05ac3a2db9a17d6b3a5106580ecf63b
Łukasz Warchoł <lukasz.warchol14@gmail.com>
2019-08-13 14:03:43 +0200

    changed form

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,2 +1,41 @@
-import Form from "../../components/Form";
-export default Form;
+import React from "react";
+import useForm from "./useForm";
+
+const Input = ({ type, name, change, placeholder }) => (
+  <label>
+    <input
+      type={type}
+      name={name}
+      onChange={change}
+      placeholder={placeholder}
+    />
+  </label>
+);
+
+const App = () => {
+  const [updateValue, submitHandler, error] = useForm({});
+
+  return (
+    <form onSubmit={submitHandler}>
+      <Input type="text" name="name" change={updateValue} placeholder="Name" />
+      <Input
+        type="email"
+        name="email"
+        change={updateValue}
+        placeholder="Email"
+      />
+      <Input
+        type="password"
+        name="password"
+        change={updateValue}
+        placeholder="Password"
+      />
+
+      {error && <p>{error}</p>}
+
+      <button>Confirm</button>
+    </form>
+  );
+};
+
+export default App;
